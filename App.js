f30cd2db73f2606c4d86852c61788d5ac01104b9
Matheus Miranda Elias <matheuscmk4@gmail.com>
2021-09-23 11:13:00 -0300

    Teste com galeria gerando fotos aleatorias

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <StatusBar hidden />
+    <WebView 
+      source={{ uri: 'https://localhost/app-galeria' }}
+    />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ // source={{ html:}}
+//´
+      
+// <html>
+// <head>
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+// </head>
+//   <body>
+//     <style>
+//       *{
+//         margin:0;
+//         padding:0;
+//         box-sizing:border-box;
+//       }
+//       .btn{
+//         width:100%;
+//         height:20px;
+//         background-color:#069;
+//         text-align:center;
+//         color:white;
+//       }
+     
+    
+//     </style>
+//     <div class="btn">Clique aqui!</div>
+//   </body>
+// </html>
+
+// ´
